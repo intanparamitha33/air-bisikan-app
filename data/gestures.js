3e592c754c7bb2bc78d4c616a68795d5f62f0e99
intanparamitha33 <intanparamitha33@gmail.com>
2025-03-28 16:11:10 +0700

    modified:   data/gestures.js 	modified:   navigation/screens/HomeScreen.js 	modified:   package-lock.json 	modified:   package.json

--- data/gestures.js
+++ data/gestures.js
@@ -1,43 +1,213 @@
 export const gestures = [
     {
-        id: "H1",
-        name: "A",
+        id: "C1",
+        name: "Contoh 1",
         video: require('../assets/info_gathering.mp4')
     },
     {
-        id: "H2",
-        name: "B",
-        video: "https://drive.google.com/uc?export=download&id=1gNtyYETN6cRyaLGA7ZcFqSClgYHKC7kB/view?usp=drive_link"
+        id: "K01",
+        name: "Aku",
+        video: "" 
     },
-    // notes: jika pakai link, masih belum bisa load videonya
+    {
+        id: "K02",
+        name: "Bicara",
+        video: "" 
+    }
+    ,
+    {
+        id: "K03",
+        name: "Cinta",
+        video: "" 
+    },
+    {
+        id: "K04",
+        name: "Kamu",
+        video: "" 
+    },
+    {
+        id: "K05",
+        name: "Kecewa",
+        video: ""
+    },
+    {
+        id: "K06",
+        name: "Kenapa",
+        video: "" 
+    },
+    {
+        id: "K07",
+        name: "Maaf",
+        video: "" 
+    },
+    {
+        id: "K08",
+        name: "Mau",
+        video: "" 
+    },
+    {
+        id: "K09",
+        name: "Sabar",
+        video: "" 
+    },
+    {
+        id: "K10",
+        name: "Sayang",
+        video: "" 
+    },
+    {
+        id: "K11",
+        name: "Sedih",
+        video: "" 
+    },
+    {
+        id: "K12",
+        name: "Semangat",
+        video: "" 
+    },
+    {
+        id: "K13",
+        name: "Senang",
+        video: "" 
+    },
+    {
+        id: "K14",
+        name: "Terima Kasih",
+        video: "" 
+    },
+    {
+        id: "K15",
+        name: "Tidak",
+        video: "" 
+    }
     // {
-    //     id: "H1",
+    //     id: "H01",
     //     name: "A",
     //     video: "https://drive.google.com/uc?export=download&id=1gNtyYETN6cRyaLGA7ZcFqSClgYHKC7kB/view?usp=drive_link"
     // },
     // {
-    //     id: "H2",
+    //     id: "H02",
     //     name: "B",
     //     video: "https://drive.google.com/uc?export=download&id=1TlCOUs0wRfMu51TTbqFY2PXJc378aFoA/view?usp=drive_link"
     // },
     // {
-    //     id: "H3",
+    //     id: "H03",
     //     name: "C",
     //     video: "https://drive.google.com/uc?export=download&id=1C9L5_9akns7eJ-Q_LKrwQg3AferfCdvq/view?usp=drive_link"
     // },
     // {
-    //     id: "H4",
+    //     id: "H04",
     //     name: "D",
     //     video: "https://drive.google.com/uc?export=download&id=1wjznHMbCIp_LFqMbXZcUa7KRWsvLSulC/view?usp=drive_link"
     // },
     // {
-    //     id: "H5",
+    //     id: "H05",
     //     name: "E",
     //     video: "https://drive.google.com/uc?export=download&id=1EAeYPM0lMmkQker6J__u3W0mUA2fBvtL/view?usp=drive_link"
     // },
     // {
-    //     id: "K1",
-    //     name: "Aku",
-    //     video: "https://drive.google.com/uc?export=download&id=1Y73y5K_Iz3Nglw0Sne2InR1pA1X3LTvC/view?usp=sharing"
-    // }
+    //     id: "H06",
+    //     name: "F",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H07",
+    //     name: "G",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H08",
+    //     name: "H",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H09",
+    //     name: "I",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H10",
+    //     name: "J",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H11",
+    //     name: "K",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H12",
+    //     name: "L",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H13",
+    //     name: "M",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H14",
+    //     name: "N",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H15",
+    //     name: "O",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H16",
+    //     name: "P",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H17",
+    //     name: "Q",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H18",
+    //     name: "R",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H19",
+    //     name: "S",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H20",
+    //     name: "T",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H21",
+    //     name: "U",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H22",
+    //     name: "V",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H23",
+    //     name: "W",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H24",
+    //     name: "X",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H25",
+    //     name: "Y",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
+    // {
+    //     id: "H26",
+    //     name: "Z",
+    //     video: require('../assets/info_gathering.mp4')
+    // },
 ]
--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,13 +1,89 @@
-import { SafeAreaView, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, Alert, Dimensions } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import { useVideoPlayer, VideoView } from 'expo-video';
 
-export default function HomeScreen() { 
-    return (
-        <SafeAreaView>
-            <Text>Ini adalah layar Bisikan App.</Text>
-        </SafeAreaView>
+// showing video on screen
+const VideoPlayer = ({ videoUrl }) => {
+    const player = useVideoPlayer(videoUrl, (player) => {
+        player.loop = false;
+        // player.play();
+    })
+
+    return(
+        <VideoView player={player} style={styles.video}
+        allowsFullscreen
+        allowsPictureInPicture />
     )
- }
+}
 
+const HomeScreen = () => {
+  const [video, setVideo] = useState(null);
+
+  const pickVideo = async () => {
+    try {
+      const res = await DocumentPicker.getDocumentAsync({
+        type: 'video/*', // Hanya video
+      });
+
+      console.log(res);
+
+      if (res.type === 'cancel') {
+        Alert.alert('Cancelled', 'Video Batal Dipilih');
+        return;
+      }
+
+      const videoData = res.assets ? res.assets[0] : res;
+
+      setVideo(videoData);
+      Alert.alert('Video Dipilih!', `File: ${videoData.name}\nUkuran: ${videoData.size} bytes`);
+    } catch (err) {
+      Alert.alert('Error', 'Gagal Pilih Video');
+      console.error(err);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Pilih Video Input" onPress={pickVideo} color='#000071' />
+      {video && (
+        <View style={styles.videoInfo}>
+          <VideoPlayer style={styles.video} videoUrl={video.uri} />
+          <Text>Video Terpilih!</Text>
+          <Text>{video.name}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  videoInfo: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  video: {
+    width: Dimensions.get("window").width * (9/10),
+    height: 220,
+    marginBottom: 20
+  }
+});
+
+export default HomeScreen;
+
+// export default function HomeScreen() { 
+//     return (
+//         <SafeAreaView>
+//             <Text>Ini adalah layar Bisikan App.</Text>
+//         </SafeAreaView>
+//     )
+//  }
  /*
 SafeAreaView -> memungkinkan tampilan sesuai di layar yang terlihat (misal, tidak di atas sekali)
 
